fab0d21f8f0813b865cfa9c5b34afd1808613cb9
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2022-12-13 22:42:17 +0900

    Update GamePadExample.cs

--- Assets/GamePadExample.cs
+++ Assets/GamePadExample.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Input;
 
 public class GamePadExample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        // ゲームパッドが接続されていないとnullになる
-        if (Gamepad.current == null) return;
-        //if (GamePadExample.current.button)
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        // ゲームパッドが接続されていないとnullになる
+        if (Gamepad.current == null) return;
 
+        if (Gamepad.current.buttonNorth.wasPressedThisFrame)
+        {
+            Debug.Log("Button Northが押された！");
+        }
+        if (Gamepad.current.buttonSouth.wasReleasedThisFrame)
+        {
+            Debug.Log("Button Southが離された！");
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GamepadSpeakerOutputType.current == null) return;
+
+        GUILayout.Label($"leftStick:{Gamepad.current.leftSticl.ReadValue()}");
+        GUILayout.Label($"buttonNorth:{Gamepad.current.buttonNorth.isPressed}");
+        GUILayout.Label($"buttonSouth: {Gamepad.current.buttonSouth.isPressed}");
+        GUILayout.Label($"buttonEast: {Gamepad.current.buttonEast.isPressed}");
+        GUILayout.Label($"buttonWest: {Gamepad.current.buttonWest.isPressed}");
+        GUILayout.Label($"leftShoulder: {Gamepad.current.leftShoulder.ReadValue()}");
+        GUILayout.Label($"leftTrigger: {Gamepad.current.leftTrigger.ReadValue()}");
+        GUILayout.Label($"rightShoulder: {Gamepad.current.rightShoulder.ReadValue()}");
+        GUILayout.Label($"rightTrigger: {Gamepad.current.rightTrigger.ReadValue()}");
     }
 }
