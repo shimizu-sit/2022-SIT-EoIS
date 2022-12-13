24cc5fb490af87396f0bb662d6eed5a5bfed1852
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2022-12-13 16:27:59 +0900

    Add README.md and GamePadExample.cs

---
+++ Assets/GamePadExample.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamePadExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゲームパッドが接続されていないとnullになる
+        if (Gamepad.current == null) return;
+
+        if (GamePadExample.current.button)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
